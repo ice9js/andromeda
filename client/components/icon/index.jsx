e00db129c630fb3314c8c92a69f506090376cc4b
Kuba Birecki <kuba.birecki@crystalplanet.io>
2023-03-11 22:31:50 +0100

    Prevent unused icons from being loaded to cut down on bundle size

--- client/components/icon/index.jsx
+++ client/components/icon/index.jsx
@@ -2,26 +2,24 @@
  * External dependencies
  */
 import React from 'react';
+
 import { dom, library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-	faFacebookF,
-	faDiscord,
-	faInstagram,
-	faPinterest,
-	faTwitter,
-} from '@fortawesome/free-brands-svg-icons';
-import {
-	faArrowLeft,
-	faArrowRight,
-	faAnglesLeft,
-	faAnglesRight,
-	faBars,
-	faEllipsisH,
-	faMagnifyingGlass,
-	faRss,
-	faTimes,
-} from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
+import { faDiscord } from '@fortawesome/free-brands-svg-icons/faDiscord';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
+import { faPinterest } from '@fortawesome/free-brands-svg-icons/faPinterest';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons/faArrowLeft';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight';
+import { faAnglesLeft } from '@fortawesome/free-solid-svg-icons/faAnglesLeft';
+import { faAnglesRight } from '@fortawesome/free-solid-svg-icons/faAnglesRight';
+import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
+import { faEllipsisH } from '@fortawesome/free-solid-svg-icons/faEllipsisH';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass';
+import { faRss } from '@fortawesome/free-solid-svg-icons/faRss';
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
 
 // Add chosen icons to the library
 library.add(
